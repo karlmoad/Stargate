f1b8667927f220b7ddc5fb413d66fa65cd6f785c
Karl <karl.moad@gmail.com>
2021-03-11 15:53:45 -0500

    adding additional program args

--- stargate.py
+++ stargate.py
@@ -7,6 +7,8 @@ def main():
     arg_parser.add_argument('-f', '--file', type=str, required=False, help='file name and path to process')
     arg_parser.add_argument('-d', '--db', type=str, required=False,
                             help='database target where data will be sent, this is the reference name given to the database during configuration')
+    arg_parser.add_argument('-t', '--table', type=str, required=False, help='name of the target table')
+    arg_parser.add_argument('-p', '--password', type=str, required=False, help="database user account password")
     arg_parser.add_argument('-s', '--sheet', type=str, required=False, help='name of worksheet where data resides')
     arg_parser.add_argument('-c', '--config', type=str,
                             help='path to configuration json file, if not specified default [$HOME/.stargate/config.json] will be used')
